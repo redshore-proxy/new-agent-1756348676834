97144b1c2f8e063330fccdb9da3cb2adf91123ff
Dannyvi <dannyvis@icloud.com>
2025-08-28 10:50:06 +0800

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-  <Route path="/" element = {< Index />} />
-           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/" element = {< Index />} />
+          <Route path="/chat" element={<ChatPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
